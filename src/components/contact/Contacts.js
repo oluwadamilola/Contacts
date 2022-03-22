c777c213d461bc4184a4741c85621295e8206823
oluwadamilola <dammyolukoju@gmail.com>
2022-03-22 19:33:01 +0100

    Feature(added edit contacts function): refactor

--- src/components/contact/Contacts.js
+++ src/components/contact/Contacts.js
@@ -1,32 +1,43 @@
 import React, { useState } from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
-import ContactList from './ContactList';
 import { usersContact } from '../../api/data';
 import CreateContactForm from './CreateContactForm';
+import ContactTable from './ContactTable';
 
 const Contacts = () => {
-    const [contacts, setContacts] = useState(usersContact)
+  const [contacts, setContacts] = useState(usersContact)
 
-const createContact = (contact) =>{
-  contact.id = contacts.lenth + 1
-  setContacts([...contacts, contact])
+  //function to add new contact to the list of contacts
+  const createContact = (contact) => {
+    contact.id = contacts.lenth + 1
+    setContacts([...contacts, contact])
+  }
+
+  //function to delete existing contacts
+  const deleteContact = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id))
+  }
+//function to edit contact
+const [editing, setEditing] = useState(false);
+const initialFormState = {id:null, firstname:'', lastname:'', email:'', phonenumber:''}
+const [activeContact, setActiveContact] =useState(initialFormState)
+const editContact = (contact) =>{
+  setEditing(true)
+  setCurrentUser({id:contact.id,firstname:contact.firstname, lastname:contact.lastname, email: contact.email, phonenumber:contact.phonenumber })
 }
+//function to update user
 
 
-
-
-
-
-  return (
+return (
     <>
-  <Container>
+      <Container> 
         <Row>
           <Col xs="7">
-            <ContactList contacts= {contacts} />
-            </Col>
-            <CreateContactForm createContact ={createContact}/>
+            <ContactTable contacts={contacts} deleteContact={deleteContact} editContact={editContact}/>
+          </Col>
+          <CreateContactForm createContact={createContact} />
         </Row>
-        </Container>
+      </Container>
 
     </>
   )
