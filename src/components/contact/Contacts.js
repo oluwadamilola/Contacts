2bb3f92f2a852872f2879f1caa80d736d3fde23a
oluwadamilola <dammyolukoju@gmail.com>
2022-03-23 02:00:40 +0100

    Feature(implemented update contact):refactor

--- src/components/contact/Contacts.js
+++ src/components/contact/Contacts.js
@@ -3,6 +3,7 @@ import { Row, Col, Container } from 'react-bootstrap';
 import { usersContact } from '../../api/data';
 import CreateContactForm from './CreateContactForm';
 import ContactTable from './ContactTable';
+import EditContactForm from './EditContactForm';
 
 const Contacts = () => {
   const [contacts, setContacts] = useState(usersContact)
@@ -17,27 +18,48 @@ const Contacts = () => {
   const deleteContact = (id) => {
     setContacts(contacts.filter((contact) => contact.id !== id))
   }
-//function to edit contact
-const [editing, setEditing] = useState(false);
-const initialFormState = {id:null, firstname:'', lastname:'', email:'', phonenumber:''}
-const [activeContact, setActiveContact] =useState(initialFormState)
-const editContact = (contact) =>{
-  setEditing(true)
-  setCurrentUser({id:contact.id,firstname:contact.firstname, lastname:contact.lastname, email: contact.email, phonenumber:contact.phonenumber })
-}
-//function to update user
 
+  //function to edit contact
+  const [editing, setEditing] = useState(false);
+  const initialFormState = { id: null, firstname: '', lastname: '', email: '', phonenumber: '' }
+  const [activeContact, setActiveContact] = useState(initialFormState)
+  const editContact = (contact) => {
+    setEditing(true)
+    setActiveContact({ id: contact.id, firstname: contact.firstname, lastname: contact.lastname, email: contact.email, phonenumber: contact.phonenumber })
+  }
 
-return (
+  //function to update contact
+  const updateContact = (id, updateContact) => {
+    setEditing(false)
+    setContacts(contacts.map((contacts.id == id ? updateContact : contacts)))
+  }
+
+  return (
     <>
-      <Container> 
-        <Row>
-          <Col xs="7">
-            <ContactTable contacts={contacts} deleteContact={deleteContact} editContact={editContact}/>
-          </Col>
-          <CreateContactForm createContact={createContact} />
-        </Row>
+
+      <Container>
+        <Col xs="7">
+          <ContactTable contacts={contacts} deleteContact={deleteContact} editContact={editContact} />
+        </Col>
+        {editing ? (
+          <Row>
+            <Col xs="4">
+              <EditContactForm
+                setEditing={setEditing}
+                activeContact={activeContact}
+                updateContact={updateContact}
+              />
+            </Col>
+          </Row>
+        ) : (
+          <Row>
+            <Col xs="4">
+              <CreateContactForm createContact={createContact} />
+            </Col>
+          </Row>
+        )}
       </Container>
+
 
     </>
   )
